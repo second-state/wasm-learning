847e26c80faaa65a7a4944fd243643b81c9895fc
Michael Yuan <michael@michaelyuan.com>
2020-04-10 18:31:02 +0000

    Delete artifacts

--- nodejs/hello-v8/node/hello_lib_bg.js
+++
@@ -1,8 +0,0 @@
-
-const path = require('path').join(__dirname, 'hello_lib_bg.wasm');
-const bytes = require('fs').readFileSync(path);
-let imports = {};
-
-const wasmModule = new WebAssembly.Module(bytes);
-const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
-module.exports = wasmInstance.exports;
