445694f866c394c847c40e586c2c71dce76829fa
Michael Yuan <michael@michaelyuan.com>
2020-05-05 05:20:13 +0000

    update tensorflow example

--- nodejs/tensorflow/node/app.js
+++ nodejs/tensorflow/node/app.js
@@ -4,12 +4,12 @@ var result = JSON.parse(infer(JSON.stringify(["mobilenet_v2_1.4_224_frozen.pb", 
 if (result[1] == 0) {
   console.log("Detected nothing");
 } else {
-  console.log("Detected object id " + result[0] + " with probability " + result[1]);
+  console.log("Detected object id " + result[1] + " with probability " + result[0]);
 }
 
 var result = JSON.parse(infer(JSON.stringify(["mobilenet_v2_1.4_224_frozen.pb", "cat.png", 224, 224])));
 if (result[1] == 0) {
   console.log("Detected nothing");
 } else {
-  console.log("Detected object id " + result[0] + " with probability " + result[1]);
+  console.log("Detected object id " + result[1] + " with probability " + result[0]);
 }
