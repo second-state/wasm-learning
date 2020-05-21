be8aee696a3a907a5d175869e5a863eacc1e0dab
Michael Yuan <michael@secondstate.io>
2020-05-21 05:48:01 +0000

    The storage example

--- nodejs/storage/src/lib.rs
+++ nodejs/storage/src/lib.rs
@@ -1,64 +1,71 @@
 use wasm_bindgen::prelude::*;
-use num_integer::lcm;
-use sha3::{Digest, Sha3_256, Keccak256};
 use serde::{Serialize, Deserialize};
+use serialize_deserialize_u8_i32::s_d_u8_i32;
+use rust_storage_interface_library::ssvm_storage;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-  x: f32, 
-  y: f32
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Line {
-  points: Vec<Point>,
-  valid: bool,
-  length: f32,
-  desc: String
+#[derive(Serialize, Deserialize, PartialEq, Debug, Default)]
+struct TestStruct {
+    a_vec: Vec<u8>,
+    a_i32: i32,
+    a_u8: u8,
+    a_bool: bool,
 }
 
 #[wasm_bindgen]
-pub fn create_line (p1: &str, p2: &str, desc: &str) -> String {
-  let point1: Point = serde_json::from_str(p1).unwrap();
-  let point2: Point = serde_json::from_str(p2).unwrap();
-  let length = ((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y)).sqrt();
-  
-  let valid = if length == 0.0 { false } else { true };
-
-  let line = Line { points: vec![point1, point2], valid: valid, length: length, desc: desc.to_string() };
-
-  return serde_json::to_string(&line).unwrap();
+pub fn store_primitive_types (data: &str) -> String {
+  let to_store: (bool, char, i64, f64) = serde_json::from_str(data).unwrap();
+  let key_0 = ssvm_storage::store::store(to_store.0);
+  let key_1 = ssvm_storage::store::store(to_store.1);
+  let key_2 = ssvm_storage::store::store(to_store.2);
+  let key_3 = ssvm_storage::store::store(to_store.3);
+  return serde_json::to_string(&(key_0, key_1, key_2, key_3)).unwrap();
 }
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  let r = String::from("hello ");
-  return r + s;
+pub fn load_primitive_types (ks: &str) -> String {
+  let keys: (i32, i32, i32, i32) = serde_json::from_str(ks).unwrap();
+  let d_0 = ssvm_storage::load::load_as_boolean(keys.0);
+  let d_1 = ssvm_storage::load::load_as_char(keys.1);
+  let d_2 = ssvm_storage::load::load_as_i64(keys.2);
+  let d_3 = ssvm_storage::load::load_as_f64(keys.3);
+  return serde_json::to_string(&(d_0, d_1, d_2, d_3)).unwrap();
 }
 
 #[wasm_bindgen]
-pub fn obfusticate(s: String) -> String {
-  (&s).chars().map(|c| {
-    match c {
-      'A' ..= 'M' | 'a' ..= 'm' => ((c as u8) + 13) as char,
-      'N' ..= 'Z' | 'n' ..= 'z' => ((c as u8) - 13) as char,
-      _ => c
-    }
-  }).collect()
+pub fn store_string (data: &str) -> i32 {
+  let key = ssvm_storage::store::store(data);
+  return key;
 }
 
 #[wasm_bindgen]
-pub fn lowest_common_denominator(a: i32, b: i32) -> i32 {
-  let r = lcm(a, b);
-  return r;
+pub fn load_string (key: i32) -> String {
+  let data = ssvm_storage::load::load_as_string(key);
+  return data;
 }
 
 #[wasm_bindgen]
-pub fn sha3_digest(v: Vec<u8>) -> Vec<u8> {
-  return Sha3_256::digest(&v).as_slice().to_vec();
+pub fn store_struct (data: &str) -> i32 {
+  let to_store: TestStruct = serde_json::from_str(data).unwrap();
+  let key = ssvm_storage::store::store(to_store);
+  return key;
 }
 
 #[wasm_bindgen]
-pub fn keccak_digest(s: &[u8]) -> Vec<u8> {
-  return Keccak256::digest(s).as_slice().to_vec();
+pub fn load_struct (key: i32) -> String {
+  let struct_skeleton = TestStruct::default();
+  let d: TestStruct = ssvm_storage::load::load_as_struct(struct_skeleton, key);
+  return serde_json::to_string(&d).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn store_bytes (data: &[u8]) -> i32 {
+  let bytes_in_i32: Vec<i32> = s_d_u8_i32::serialize_u8_to_i32((*data).to_vec());
+  let key = ssvm_storage::store::store(bytes_in_i32);
+  return key;
+}
+
+#[wasm_bindgen]
+pub fn load_bytes (key: i32) -> Vec<u8> {
+  let bytes_in_i32: Vec<i32> = ssvm_storage::load::load_as_i32_vector(key);
+  return s_d_u8_i32::deserialize_i32_to_u8(bytes_in_i32);
 }
