84ca552a4f93c0fa69d82c1c62ad0ad60ea8332b
Michael Yuan <michael@michaelyuan.com>
2020-06-05 00:49:14 +0000

    clarify wasi example

--- nodejs/wasi/src/lib.rs
+++ nodejs/wasi/src/lib.rs
@@ -9,9 +9,15 @@ pub fn get_random_i32() -> i32 {
 
 #[wasm_bindgen]
 pub fn get_random_bytes() -> Vec<u8> {
+  let mut rng = thread_rng();
+  let mut arr = [0u8; 128];
+  rng.fill(&mut arr[..]);
+  return arr.to_vec();
+  /*
   let mut vec: Vec<u8> = vec![0; 128];
   getrandom::getrandom(&mut vec).unwrap();
   return vec;
+   */
 }
 
 #[wasm_bindgen]
