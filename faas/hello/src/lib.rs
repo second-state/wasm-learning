d84a10223dac7ec4e0bdee70a4ff66342856658a
Michael Yuan <michael@michaelyuan.com>
2020-07-07 00:42:34 +0000

    updatedemo

--- faas/hello/src/lib.rs
+++ faas/hello/src/lib.rs
@@ -1,12 +1,7 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(context: &str, s: &str) -> String {
-  if context == "emoji" {
-    let r = String::from("👋 ");
-    return r + &s;
-  } else {
-    let r = String::from("hello ");
-    return r + &s;
-  }
+pub fn say(s: &str) -> String {
+  let r = String::from("hello ");
+  return r + s;
 }
