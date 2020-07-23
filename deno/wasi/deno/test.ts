771cd07ab8aea5ed5f866eb6091e41e3caf43fe7
Michael Yuan <michael@michaelyuan.com>
2020-07-23 22:50:51 +0000

    attempt to add init code block to deno wasi

--- deno/wasi/deno/test.ts
+++ deno/wasi/deno/test.ts
@@ -1,6 +1,8 @@
-import { get_random_i32, get_random_bytes, echo, print_env, create_file } from '../pkg/wasi_example_lib.js';
+import { get_random_i32, echo, print_env, create_file, read_file, del_file } from '../pkg/wasi_example_lib.js';
 
 console.log( "My random number is: ", get_random_i32() );
 echo("Hello Deno");
 print_env();
-// create_file("hello.txt", "Hello WASI");
+create_file("/hello.txt", "Hello WASI");
+console.log( read_file("/hello.txt") );
+del_file("/hello.txt");
