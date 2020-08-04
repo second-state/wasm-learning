41e4208d2aea7bd1f9c48880122cce3a38032f69
hydai <hydai@secondstate.io>
2020-08-04 12:57:15 +0000

    Update rust-rsa-example, due to ssvm-js api changes

--- ssvm/rust-rsa-example/lib.js
+++ ssvm/rust-rsa-example/lib.js
@@ -1,13 +1,9 @@
 let vm;
-/**
-* @param {number} bits
-* @returns {string}
-*/
-module.exports.generate_key_pair = function(bits) {
-	return vm.Run("_start", "generate_key_pair", bits);
+module.exports.generate_key_pair = function() {
+	return vm.Start();
 };
 
 const ssvm = require('ssvm');
 const path = require('path').join(__dirname, 'target/wasm32-wasi/release/rust-rsa-example.wasm');
 
-vm = new ssvm.VM(path, {"DisableWasmBindgen": true});
+vm = new ssvm.VM(path, {"EnableWasiStartFunction": true, env: process.env, args: process.argv, preopens:{'/': __dirname}});
