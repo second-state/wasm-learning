0f7ce1da0232864c88539c6426522f4630fbbeaa
tpmccallum <mistermac2008@gmail.com>
2020-12-11 10:12:41 +1000

    Update media files

--- faas/video_inference/test/test.js
+++
@@ -1,8 +0,0 @@
-const { infer } = require('../pkg/mobilenet_service_lib.js');
-const fs = require('fs');
-
-console.time("Image Classification");
-var img_src = fs.readFileSync("food.jpg");
-console.timeLog("Image Classification");
-console.log("Result is: ", infer(img_src));
-console.timeEnd("Image Classification");
