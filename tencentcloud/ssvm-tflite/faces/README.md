83e73fbdc6c792cbf145b8720a21b5c5bf6c29f6
Michael Yuan <michael@secondstate.io>
2020-12-20 16:58:22 +0000

    add face detection example for tencent cloud

---
+++ tencentcloud/ssvm-tflite/faces/README.md
@@ -0,0 +1,57 @@
+
+## Prerequisite
+
+[Install the ssvmup tool](https://www.secondstate.io/articles/ssvmup/)
+
+## Build
+
+```
+$ ssvmup build --enable-aot
+```
+
+## Test
+
+Create the layer for tensorflow and SSVM binaries.
+
+```
+$ cd ../layer
+$ source download_dependencies.sh
+```
+
+Run the wasm application.
+
+```
+$ LD_LIBRARY_PATH=../layer ../layer/ssvm-tensorflow pkg/mtcnn.wasm < html/solvay.json
+$ LD_LIBRARY_PATH=../layer ../layer/ssvm-tensorflow pkg/mtcnn.so < html/solvay.json
+```
+
+## Deploy
+
+Deploy the layer on TencentCloud SCF by uploading the `layer.zip` file via COS.
+
+```
+$ cd layer
+$ zip --symlinks -r layer.zip *
+```
+
+Deploy the function by uploading the `mtcnn.zip` file to the function service.
+
+```
+$ cd cloud
+$ cp ../pkg/mtcnn.so .
+$ zip -r mtcnn.zip *
+```
+
+Create and connect an API gateway to the function and turn on CORS.
+
+## Live test
+
+```
+$ base64 -w 0 html/solvay.jpg | curl -d @- -X POST https://service-lty62pd6-1302315972.hk.apigw.tencentcs.com/release/tflite_food
+```
+
+## Jamstack test
+
+Upload the `html` files to a static file host. Example: https://tensorflow-demo-0gbniz1314770b82-1302315972.tcloudbaseapp.com/food/
+
+
---
+++ tencentcloud/ssvm-tflite/faces/src/main.rs
@@ -0,0 +1,69 @@
+use std::io::{self, Read};
+use ssvm_tensorflow_interface;
+use serde::Deserialize;
+use image::{GenericImageView, Pixel};
+use imageproc::drawing::draw_hollow_rect_mut;
+use imageproc::rect::Rect;
+
+fn main() {
+    let model_data: &[u8] = include_bytes!("mtcnn.pb");
+
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).expect("Error reading from STDIN");
+    let obj: FaasInput = serde_json::from_str(&buffer).unwrap();
+    // println!("{} {}", &(obj.body)[..5], obj.body.len());
+    let img_buf = base64::decode_config(&(obj.body), base64::STANDARD).unwrap();
+    // println!("Image buf size is {}", img_buf.len());
+
+    let mut img = image::load_from_memory(&img_buf).unwrap();
+    let mut flat_img: Vec<f32> = Vec::new();
+    for (_x, _y, rgb) in img.pixels() {
+        flat_img.push(rgb[2] as f32);
+        flat_img.push(rgb[1] as f32);
+        flat_img.push(rgb[0] as f32);
+    }
+
+    let mut session = ssvm_tensorflow_interface::Session::new(&model_data, ssvm_tensorflow_interface::ModelType::TensorFlow);
+    session.add_input("min_size", &[20.0f32], &[])
+           .add_input("thresholds", &[0.6f32, 0.7f32, 0.7f32], &[3])
+           .add_input("factor", &[0.709f32], &[])
+           .add_input("input", &flat_img, &[img.height().into(), img.width().into(), 3])
+           .add_output("box")
+           .add_output("prob")
+           .run();
+    let res_vec: Vec<f32> = session.get_output("box");
+
+    // Parse results.
+    let mut iter = 0;
+    let mut box_vec: Vec<[f32; 4]> = Vec::new();
+    while (iter * 4) < res_vec.len() {
+        box_vec.push([
+            res_vec[4 * iter + 1], // x1
+            res_vec[4 * iter],     // y1
+            res_vec[4 * iter + 3], // x2
+            res_vec[4 * iter + 2], // y2
+        ]);
+        iter += 1;
+    }
+
+    println!("Drawing box: {} results ...", box_vec.len());
+    let line = Pixel::from_slice(&[0, 255, 0, 0]);
+    for i in 0..box_vec.len() {
+        let xy = box_vec[i];
+        let x1: i32 = xy[0] as i32;
+        let y1: i32 = xy[1] as i32;
+        let x2: i32 = xy[2] as i32;
+        let y2: i32 = xy[3] as i32;
+        let rect = Rect::at(x1, y1).of_size((x2 - x1) as u32, (y2 - y1) as u32);
+        draw_hollow_rect_mut(&mut img, rect, *line);
+    }
+
+    let mut buf = Vec::new();
+    img.write_to(&mut buf, image::ImageOutputFormat::Jpeg(80u8)).expect("Unable to write");
+    println!("{}", base64::encode_config(buf, base64::STANDARD));
+}
+
+#[derive(Deserialize, Debug)]
+struct FaasInput {
+    body: String
+}
