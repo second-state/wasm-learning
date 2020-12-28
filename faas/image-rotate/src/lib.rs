3873d2921bb4dfe16585a453cef3ed030ec4f9f4
tpmccallum <mistermac2008@gmail.com>
2020-12-28 14:44:47 +1000

    Compressed example file

--- faas/image-rotate/src/lib.rs
+++ faas/image-rotate/src/lib.rs
@@ -2,7 +2,6 @@ use image::Rgba;
 use image::ImageBuffer;
 use image::DynamicImage;
 use wasm_bindgen::prelude::*;
-use std::convert::TryInto;
 use image::{GenericImageView, ImageFormat};
 use imageproc::geometric_transformations::Interpolation;
 use imageproc::geometric_transformations::rotate_about_center;
@@ -24,17 +23,31 @@ pub fn rotate_an_image(img_buf: &[u8]) -> Vec<u8> {
     println!("Converting DynamicImage to ImageBuffer ...");
     let image_bytes = DynamicImage::into_bytes(dyn_img);
     let image_buffer = ImageBuffer::<Rgba<u8>, Vec<u8>>::from_vec(w, h, image_bytes[..].to_vec()).unwrap();
-    println!("Rotating image ...");
-    let rotated_image: ImageBuffer::<Rgba<u8>, Vec<u8>> = rotate_about_center(&image_buffer, -0.095, Interpolation::Nearest, Rgba([255, 0, 0, 0]));
-    println!("Image rotated!");
+    println!("TESTING - NOT Rotating image ...");
+    let unrotated_rotated_image_as_arr: &[u8] = &image_buffer.into_vec();
+
+    //println!("Rotating image ...");
+    //let rotated_image: ImageBuffer::<Rgba<u8>, Vec<u8>> = rotate_about_center(&image_buffer, -0.095, Interpolation::Nearest, Rgba([255, 0, 0, 0]));
+    //println!("Image rotated!");
     let mut buf = vec![];
     println!("Creating byte array to return");
-    let img_to_write = match image::load_from_memory_with_format(&rotated_image.as_raw(), image::ImageFormat::Png) {
-        Ok(i) => println!("OK"),
+    match image::load_from_memory_with_format(unrotated_rotated_image_as_arr, image::ImageFormat::Png) {
+        Ok(img_to_write) => {
+            println!("Writing image data ...");
+            img_to_write.write_to(&mut buf, image::ImageOutputFormat::Png).unwrap();
+        }
         Err(e) => println!("Error: {:?}", &e.to_string()),
     };
-    println!("Writing ...");
-    img_to_write.write_to(&mut buf, image::ImageOutputFormat::Png).unwrap();
-    println!("Returning byte array now");
+
+    //let rotated_image_as_arr: &[u8] = &rotated_image.into_vec();
+    //println!("{:?}", rotated_image_as_arr);
+    //match image::load_from_memory_with_format(rotated_image_as_arr, image::ImageFormat::Png) {
+    //    Ok(img_to_write) => {
+    //        println!("Writing image data ...");
+    //        img_to_write.write_to(&mut buf, image::ImageOutputFormat::Png).unwrap();
+    //    }
+    //    Err(e) => println!("Error: {:?}", &e.to_string()),
+    //};
+    println!("Returning ...");
     return buf;
 }
