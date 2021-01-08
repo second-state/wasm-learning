8bae5de48f213b965d0cf2c97466d6d3e3ae42fa
tpmccallum <mistermac2008@gmail.com>
2021-01-08 15:28:38 +1000

    Update OCR

--- faas/ocr/src/lib.rs
+++ faas/ocr/src/lib.rs
@@ -10,13 +10,13 @@ pub fn ocr(img_buf: &[u8]) -> String {
     _initialize();
     match image::load_from_memory(img_buf) {
         Ok(img_to_write) => {
-            img_to_write.save_with_format("/temp.png", image::ImageFormat::Png);
+            img_to_write.save_with_format("/temp_input.png", image::ImageFormat::Png);
         }
         Err(e) => println!("Error: {:?}", &e.to_string()),
     };
     let mut cmd = Command::new("tesseract");
     cmd.arg("/temp_input.png").arg("/temp_output");
-    let mut f = File::open("/temp.txt").unwrap();
+    let mut f = File::open("/temp_output.txt").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
     s
