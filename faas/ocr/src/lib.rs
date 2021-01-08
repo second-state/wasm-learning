4c9939658a3db446324f6cef2148267aa8d6d0f6
tpmccallum <mistermac2008@gmail.com>
2021-01-08 16:38:03 +1000

    Ensure that std fs is the only mechanism that writes to fs

--- faas/ocr/src/lib.rs
+++ faas/ocr/src/lib.rs
@@ -3,19 +3,24 @@ use ssvm_wasi_helper::ssvm_wasi_helper::_initialize;
 use std::fs::File;
 use std::io::Read;
 use std::str;
+use std::io::prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn ocr(img_buf: &[u8]) -> String {
+pub fn ocr(data: &[u8]) -> String {
     _initialize();
-    match image::load_from_memory(img_buf) {
-        Ok(img_to_write) => {
-            img_to_write.save_with_format("/temp_input.png", image::ImageFormat::Png);
-        }
-        Err(e) => println!("Error: {:?}", &e.to_string()),
-    };
+    let mut pos = 0;
+    let mut buffer = File::create("/temp_input.png").unwrap();
+
+    while pos < data.len() {
+        let bytes_written = buffer.write(&data[pos..]).unwrap();
+        pos += bytes_written;
+    }
     let mut cmd = Command::new("tesseract");
     cmd.arg("/temp_input.png").arg("stdout");
     let out = cmd.output();
+    println!("Code: {}", out.status);
+    println!("STDERR: {}", str::from_utf8(&out.stderr).unwrap());
+    println!("STDOUT: {}", str::from_utf8(&out.stdout).unwrap());
     str::from_utf8(&out.stdout).unwrap().to_string()
 }
