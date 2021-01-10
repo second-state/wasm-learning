deac217ee9c3adc7f81cb1fbccd62f66fb717d4f
tpmccallum <mistermac2008@gmail.com>
2021-01-10 15:07:05 +1000

    Adding multi-language support for OCR

--- faas/ocr/src/lib.rs
+++ faas/ocr/src/lib.rs
@@ -1,25 +1,36 @@
 use rust_process_interface_library::Command;
 use ssvm_wasi_helper::ssvm_wasi_helper::_initialize;
+use std::fs;
 use std::fs::File;
-use std::str;
 use std::io::prelude::*;
+use std::str;
+use std::time::{SystemTime, UNIX_EPOCH};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn ocr(data: &[u8]) -> String {
+pub fn ocr(data: &[u8], language: &str,) -> String {
     _initialize();
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let mut guest_temp_input_filename = String::from("/");
+    guest_temp_input_filename.push_str(&now.as_millis().to_string());
+    let copy_of_guest_temp_input_filename = guest_temp_input_filename.clone();
     let mut pos = 0;
-    let mut buffer = File::create("/temp_input.png").unwrap();
-
+    let mut buffer = File::create(guest_temp_input_filename).unwrap();
     while pos < data.len() {
         let bytes_written = buffer.write(&data[pos..]).unwrap();
         pos += bytes_written;
     }
+    let mut host_temp_input_filename = String::from("/tmp/");
+    host_temp_input_filename.push_str(&now.as_millis().to_string());
+    let copy_of_host_temp_input_filename = host_temp_input_filename.clone();
     let mut cmd = Command::new("tesseract");
-    cmd.arg("/tmp/temp_input.png").arg("stdout").arg("--dpi").arg("70");
+    cmd.arg(&copy_of_host_temp_input_filename)
+        .arg("stdout")
+        .arg("--dpi")
+        .arg("70")
+        .arg("-l")
+        .arg(language);
     let out = cmd.output();
-    println!("Code: {}", out.status);
-    println!("STDERR: {}", str::from_utf8(&out.stderr).unwrap());
-    println!("STDOUT: {}", str::from_utf8(&out.stdout).unwrap());
+    fs::remove_file(&copy_of_guest_temp_input_filename).unwrap();
     str::from_utf8(&out.stdout).unwrap().to_string()
 }
