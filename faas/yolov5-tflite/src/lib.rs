af7bd690cc3f8740541972befe2f1a448d80a006
tpmccallum <mistermac2008@gmail.com>
2021-07-26 09:33:10 +1000

    Test input values

--- faas/yolov5-tflite/src/lib.rs
+++ faas/yolov5-tflite/src/lib.rs
@@ -8,17 +8,15 @@ use std::time::{Instant};
 
 #[wasm_bindgen]
 pub fn detect(image_data: &[u8]) -> Vec<u8> {
-    println!("* {:?}", image_data);
     let start = Instant::now();
     let mut img = image::load_from_memory(image_data).unwrap();
-    println!("** {:?}", img);
     let resized = image::imageops::thumbnail(&img, 320, 320);
     println!("Resized image in ... {:?}", start.elapsed());
     let mut flat_img: Vec<f32> = Vec::new();
     for rgb in resized.pixels() {
-        flat_img.push(rgb[0] as f32 / 255.);
-        flat_img.push(rgb[1] as f32 / 255.);
         flat_img.push(rgb[2] as f32 / 255.);
+        flat_img.push(rgb[1] as f32 / 255.);
+        flat_img.push(rgb[0] as f32 / 255.);
     }
     println!("Flat image:");
     println!("{:?}", flat_img);
