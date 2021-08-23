6afaf496d9c37b1e23b084c0030dfe3fa94b6a4e
tpmccallum <mistermac2008@gmail.com>
2021-08-23 14:59:43 +1000

    Trying different examples of text now, works great

--- faas/tf_lite_natural_language/src/lib.rs
+++ faas/tf_lite_natural_language/src/lib.rs
@@ -10,13 +10,16 @@ pub fn classify(input_string: &str, data: &str) -> String {
     // Blank for now
     let mut map_name: HashMap<String, i32> = HashMap::new();
     println!("Input string: {:?}", input_string);
-    println!("Data: {:?}", data);
+    //println!("Data: {:?}", data);
 
-    let individualLine: Vec<&str> = data.split(|c| c == '\n').collect();
+    // Lower case
+    let lower_data: &str = &data.to_lowercase();
+
+    let individualLine: Vec<&str> = lower_data.split(|c| c == '\n').collect();
 
     //println!("individualLine: {:?}", individualLine);
     for text in individualLine {
-        println!("Processing text: {:?}", text);
+        //println!("Processing text: {:?}", text);
         let v: Vec<&str> = text.split(|c| c == '|').collect();
         if (v.len() > 1) {
             map_name.insert(v[0].to_string(), v[1].parse::<i32>().unwrap());
@@ -28,7 +31,8 @@ pub fn classify(input_string: &str, data: &str) -> String {
     let mut vecForModel: Vec<i32> = Vec::with_capacity(size);
 
     // Build the input string vec by splitting on space
-    let vInputString: Vec<&str> = input_string.split(|c| c == ' ').collect();
+    let lower_input_string: &str = &input_string.to_lowercase();
+    let vInputString: Vec<&str> = lower_input_string.split(|c| c == ' ').collect();
     for word in vInputString {
         println!("Processing word: {:?}", word);
         if (map_name.contains_key(word) && vecForModel.len() < 256) {
@@ -42,6 +46,7 @@ pub fn classify(input_string: &str, data: &str) -> String {
     println!("Final Vec For Model: {:?}", vecForModel);
     if (vecForModel.len() > 0) {
         // Load in the model
+        println!("Processing loading the model");
         let model_data: &[u8] = include_bytes!("model.tflite");
 
         // Create a TF Lite session
@@ -59,22 +64,24 @@ pub fn classify(input_string: &str, data: &str) -> String {
         session.add_input("input_1", &vecForModel[..], &[1, 256]);
 
         // Print the input for testing purposes
-        println!("{:?}", &vecForModel[..]);
+        //println!("{:?}", &vecForModel[..]);
 
         // Specify the output
+        println!("Adding output");
         session.add_output("Identity");
 
         // Run
+        println!("Running session");
         session.run();
 
         // Fetch results as f32
         let tf_lite_result_as_f32: Vec<f32> = session.get_output("Identity");
         println!("Result length is: {:?}", tf_lite_result_as_f32.len());
-        stringResult.push_str("The words you provided have a ");
+        stringResult.push_str("The words you provided have a negative connotation index of ");
         stringResult.push_str(&tf_lite_result_as_f32[0].to_string());
-        stringResult.push_str(" negative connotation, and a ");
+        stringResult.push_str(", and a positive connotation index of ");
         stringResult.push_str(&tf_lite_result_as_f32[1].to_string());
-        stringResult.push_str(" positive connotation.");
+        stringResult.push_str(".");
     } else {
         stringResult.push_str("None of the words you provided were recognised :(");
     }
